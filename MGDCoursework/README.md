8529d8e702acd3a517e244b2066493ad774ae89b
benmadine <benmadine@gmail.com>
2019-04-11 18:49:42 +0100

    Fianl

---
+++ MGDCoursework/README.md
@@ -0,0 +1,5 @@
+# MGDCoursework
+This application displays the past 100 earthquakes that have been recorded by the British Geological Survey using XML parsing. 
+This application was developed using Java, XML and AndroidStudio.
+
+Currently WIP.
---
+++ TestApplication1/app/src/main/java/labstuff/gcu/me/org/testapplication1/MainActivity.java
@@ -0,0 +1,14 @@
+package labstuff.gcu.me.org.testapplication1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
